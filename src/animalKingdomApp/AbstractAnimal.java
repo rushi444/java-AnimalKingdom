60681cdc0344c7c1cbe2fef6e0188b45caf05315
rushi444 <rushi44@gmail.com>
2019-07-10 12:47:05 -0700

    Mammal class added

--- src/animalKingdomApp/AbstractAnimal.java
+++ src/animalKingdomApp/AbstractAnimal.java
@@ -1,24 +1,26 @@
 package animalKingdomApp;
 
 public abstract class AbstractAnimal {
-    private static int maxNumber = 0; 
+    private static int maxNumber = 0;
     private int number;
     private String name;
     private int year;
 
-    public void eatFood() {
-        System.out.println("Eating Food...");
-    }
-
-    public abstract String move(); 
-    public abstract String breath(); 
-    public abstract String reproduce(); 
-
     public AbstractAnimal(String name, int year) {
-        maxNumber++; 
-        number = maxNumber; 
-        this.name = name; 
-        this.year = year; 
+        maxNumber++;
+        number = maxNumber;
+        this.name = name;
+        this.year = year;
     }
+
+    public String eatFood() {
+        return "Eating Food...";
+    }
+
+    public abstract String move();
+
+    public abstract String breath();
+
+    public abstract String reproduce();
 
 }
---
+++ src/animalKingdomApp/Mammal.java
@@ -0,0 +1,21 @@
+package animalKingdomApp;
+
+public class Mammal extends AbstractAnimals {
+    public Mammal(String name, int year) {
+        super(name, year);
+    }
+
+    @Override
+    public String move() {
+        return "walk"; 
+    }
+    @Override
+    public String breath() {
+        return "lungs"; 
+    }
+    @Override 
+    public String reproduce(){
+        return "live births"; 
+    }
+
+}
